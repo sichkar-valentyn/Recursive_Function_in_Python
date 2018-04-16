cca0ff6811f8369aef8dd5208706293ae9ac8d19
Valentyn N Sichkar <34546286+sichkar-valentyn@users.noreply.github.com>
2018-04-16 21:09:40 +0300

    Update Recursive_Function_in_Python.py

--- Recursive_Function_in_Python.py
+++ Recursive_Function_in_Python.py
@@ -1,3 +1,14 @@
+# File: Recursive_Function_in_Python.py
+# Description: Using recursive function to calculate Fibonacci value
+# Environment: PyCharm and Anaconda environment
+#
+# MIT License
+# Copyright (c) 2018 Valentyn N Sichkar
+# github.com/sichkar-valentyn
+#
+# Reference to:
+# [1] Valentyn N Sichkar. Using recursive function to calculate Fibonacci value // GitHub platform [Electronic resource]. URL: https://github.com/sichkar-valentyn/Recursive_Function_in_Python (date of access: XX.XX.XXXX)
+
 # Implementing task to calculate Fibonacci value
 def fib(x):
     if x == 0 or x == 1:
